5189c441d463e297390d45a75ab719e9a6d51228
cpop8 <cpop8@colostate.edu>
2020-11-17 22:24:34 -0700

    deployment prep

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 //const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const cors = require('cors')
 
 const db = require('./dbConnectExec.js')
 const config = require('./config.js')
 const auth = require('./middleware/authenticate')
 
+//azurewebsites.net, colostate.edu
 const app = express();
 app.use(express.json())
-//app.use(cors())
+app.use(cors())
 
 //const auth = async(req, res, next)=>{
     //console.log(req.header('Authorization'))
@@ -185,4 +187,6 @@ app.get("/appointment/:pk", (req, res)=>{
         res.status(500).send()
     })
 })
-app.listen(5000,()=>{console.log("app is running on port 5000")})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{console.log(`app is running on port ${PORT}`)})
